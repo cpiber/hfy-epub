6a4c36f71fcf1a57952953725ed8a4a67ea39868
Constantin Piber <cp.piber@gmail.com>
2023-07-22 13:53:55 +0200

    [#1] Introduce API TMR warning

---
+++ src/Components/Alert.svelte
@@ -0,0 +1,20 @@
+<style lang="postcss">
+  @import '../loading';
+  @include info;
+
+  .alert {
+    padding: 8px 16px;
+    border-radius: 6px;
+    border: 1px solid;
+  }
+  .alert.info {
+    border-color: color-mod($info blend(black 20%));
+    color: black;
+    background-color: color-mod($info blend(white 20%));
+  }
+</style>
+
+
+<p class="info alert">
+  <slot />
+</p>
--- src/Header.svelte
+++ src/Header.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  import Alert from './Components/Alert.svelte';
+</script>
+
 <style lang="postcss">
   header {
     margin-bottom: 2.5rem;
@@ -16,4 +20,5 @@
 
 <header>
   <h1>r/HFY epub generator <a class="small" href="https://github.com/cpiber/hfy-epub/tree/master/docs/index.md" target="_blank">Help</a></h1>
+  <Alert>Due to <a href="https://www.redditinc.com/blog/apifacts" target="_blank">recent API changes on reddit</a>, creating ebooks has become limited. If you run into a "Too many requests" error, please wait 10 minutes before trying again.</Alert>
 </header>
